784ee3ece48647891c333ca9f5151804fd8010b5
Urban Pettersson <urbanpe88@gmail.com>
2021-11-21 11:06:09 +0100

    feat: add first version of tagging functionality

---
+++ lib/tags.ts
@@ -0,0 +1,34 @@
+import { Construct, Tags } from '@aws-cdk/core'
+
+/**
+ * Available Backup plans. Availability depends on your agreement with Enfo
+ */
+export enum BackupPlan {
+  STANDARD = 'Standard',
+  STOCKHOLM = 'StandardCrossRegionStockholm',
+  IRELAND = 'StandardCrossRegionIreland',
+  FRANKFURT = 'StandardCrossRegionFrankfurt'
+}
+
+/**
+ * Tags a CDK Construct to enable Enfo Standard Backup.
+ * If an a stack is supplied this will be applied to all resources within the stack.
+ *
+ * Backups only applies to databases. The following types of resources will be affected:
+ * Aurora
+ * RDS
+ * DynamoDB
+ * EBS
+ * EC2
+ * EFS
+ * FSx
+ * Storage Gateway
+ * DocumentDB
+ * Neptune
+ *
+ * @param construct
+ * A CDK Construct
+ */
+export const tagConstructForBackup = (construct: Construct, plan: BackupPlan = BackupPlan.STANDARD): void => {
+  Tags.of(construct).add('BackupPlan', plan)
+}
