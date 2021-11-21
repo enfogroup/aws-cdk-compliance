c59a0673c9077c51c6fcb6c7b701e1e2e9bf7eb1
Urban Pettersson <urbanpe88@gmail.com>
2021-11-21 14:21:47 +0100

    test: write tests for S3Defaults

---
+++ test/s3.test.ts
@@ -0,0 +1,99 @@
+// to be tested
+import { S3Defaults } from '../lib/s3'
+
+// tools
+import '@aws-cdk/assert/jest'
+import { Stack } from '@aws-cdk/core'
+import { Bucket } from '@aws-cdk/aws-s3'
+import { Template, Match } from '@aws-cdk/assertions'
+
+describe('S3', () => {
+  describe('S3Defaults', () => {
+    it('should enforce SSL', () => {
+      const stack = new Stack()
+
+      new Bucket(stack, 'Bucket', {
+        ...S3Defaults
+      })
+
+      const template = Template.fromStack(stack)
+      template.hasResourceProperties('AWS::S3::BucketPolicy', Match.objectLike({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: 's3:*',
+              Condition: {
+                Bool: {
+                  'aws:SecureTransport': 'false'
+                }
+              },
+              Effect: 'Deny',
+              Principal: {
+                AWS: '*'
+              },
+              Resource: [
+                {
+                  'Fn::GetAtt': [
+                    Match.anyValue(),
+                    'Arn'
+                  ]
+                },
+                {
+                  'Fn::Join': [
+                    '',
+                    [
+                      {
+                        'Fn::GetAtt': [
+                          Match.anyValue(),
+                          'Arn'
+                        ]
+                      },
+                      '/*'
+                    ]
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }))
+    })
+
+    it('should set encryption', () => {
+      const stack = new Stack()
+
+      new Bucket(stack, 'Bucket', {
+        ...S3Defaults
+      })
+
+      expect(stack).toHaveResource('AWS::S3::Bucket', {
+        BucketEncryption: {
+          ServerSideEncryptionConfiguration: [
+            {
+              ServerSideEncryptionByDefault: {
+                SSEAlgorithm: 'AES256'
+              }
+            }
+          ]
+        }
+      })
+    })
+
+    it('should block public access', () => {
+      const stack = new Stack()
+
+      new Bucket(stack, 'Bucket', {
+        ...S3Defaults
+      })
+
+      expect(stack).toHaveResource('AWS::S3::Bucket', {
+        PublicAccessBlockConfiguration: {
+          BlockPublicAcls: true,
+          BlockPublicPolicy: true,
+          IgnorePublicAcls: true,
+          RestrictPublicBuckets: true
+        }
+      })
+    })
+  })
+})
