bf10a9071606fa5cde8c1a7c6e0571be2c806d4d
Urban Pettersson <urbanpe88@gmail.com>
2021-11-21 14:36:04 +0100

    fix: make S3Defaults readonly

--- lib/s3.ts
+++ lib/s3.ts
@@ -4,7 +4,7 @@ import { BlockPublicAccess, BucketEncryption, BucketProps } from '@aws-cdk/aws-s
  * Default settings for S3. Should be spread into S3 Bucket object as a part of creation.
  * See README for examples
  */
-export const S3Defaults: Pick<BucketProps, 'enforceSSL' | 'blockPublicAccess' | 'encryption'> = {
+export const S3Defaults: Readonly<Pick<BucketProps, 'enforceSSL' | 'blockPublicAccess' | 'encryption'>> = {
   enforceSSL: true,
   blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
   encryption: BucketEncryption.S3_MANAGED
