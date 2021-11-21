63e7c10723fddaf61dd93390f134741cf808da4f
Urban Pettersson <urbanpe88@gmail.com>
2021-11-21 11:09:03 +0100

    feat: add S3 defaults functionality

---
+++ lib/s3.ts
@@ -0,0 +1,11 @@
+import { BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
+
+/**
+ * Default settings for S3. Should be spread into S3 Bucket object as a part of creation.
+ * See README for example
+ */
+export const S3Defaults: Pick<BucketProps, 'enforceSSL' | 'publicReadAccess' | 'encryption'> = {
+  enforceSSL: true,
+  publicReadAccess: false,
+  encryption: BucketEncryption.S3_MANAGED
+}
