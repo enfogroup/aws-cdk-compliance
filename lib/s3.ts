e4b3a56d643ffe0325b3b1760c46cf60fd7d01b5
Urban Pettersson <urbanpe88@gmail.com>
2021-11-21 14:16:21 +0100

    fix: correct improper setting on S3Defaults

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,11 +1,11 @@
-import { BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
+import { BlockPublicAccess, BucketEncryption, BucketProps } from '@aws-cdk/aws-s3'
 
 /**
  * Default settings for S3. Should be spread into S3 Bucket object as a part of creation.
  * See README for examples
  */
-export const S3Defaults: Pick<BucketProps, 'enforceSSL' | 'publicReadAccess' | 'encryption'> = {
+export const S3Defaults: Pick<BucketProps, 'enforceSSL' | 'blockPublicAccess' | 'encryption'> = {
   enforceSSL: true,
-  publicReadAccess: false,
+  blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
   encryption: BucketEncryption.S3_MANAGED
 }
