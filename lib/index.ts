4b4b5ae31059022480ec1656fcd154b8b03d3c70
Urban Pettersson <urbanpe88@gmail.com>
2021-11-24 19:42:21 +0100

    chore: export sns functionality

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { BackupPlan, enableBackups } from './tags'
 export { compliantBucketProps, Bucket } from './s3'
 export { compliantKeyProps, Key } from './kms'
+export { TopicProps, Topic } from './sns'
