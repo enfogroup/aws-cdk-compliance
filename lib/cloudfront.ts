eef63dfbc24131b3c1e27dfa464e44e54f797639
Calle Kabo <calle@kabo.nu>
2021-11-29 15:19:01 +1300

    feat: cloudfront

---
+++ lib/cloudfront.ts
@@ -0,0 +1,45 @@
+import {
+  BehaviorOptions as CFBehaviorOptions,
+  Distribution as CFDistribution,
+  DistributionProps as CFDistributionProps,
+  ViewerProtocolPolicy as CFViewerProtocolPolicy
+} from '@aws-cdk/aws-cloudfront'
+import { Construct } from '@aws-cdk/core'
+
+export interface BehaviorOptions extends CFBehaviorOptions {
+  readonly viewerProtocolPolicy: Exclude<CFViewerProtocolPolicy, CFViewerProtocolPolicy.ALLOW_ALL>
+}
+
+export interface DistributionProps extends CFDistributionProps {
+  readonly defaultBehavior: BehaviorOptions
+  readonly additionalBehaviors?: Record<string, BehaviorOptions>
+}
+
+interface InternalDistributionProps extends DistributionProps {
+  readonly defaultRootObject: string
+  readonly enableLogging: true
+  readonly webAclId: string
+}
+
+/**
+ * Properties for a new Compliant CloudFront Distribution
+ */
+export const defaultDistributionProps = {
+  defaultRootObject: 'index.html',
+  enableLogging: true
+}
+
+/**
+ * Compliant CloudFront Distribution
+ *
+ * See README for usage examples
+ */
+export class Distribution extends CFDistribution {
+  // eslint-disable-next-line no-useless-constructor
+  constructor (scope: Construct, id: string, props?: DistributionProps) {
+    super(scope, id, {
+      ...defaultDistributionProps,
+      ...props
+    } as InternalDistributionProps)
+  }
+}
