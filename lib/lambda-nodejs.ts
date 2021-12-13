a4a3548d7ca3443e55a736cb783983f15a2735dc
Calle Kabo <calle@kabo.nu>
2021-12-13 17:08:13 +1300

    feat: nodejs function

---
+++ lib/lambda-nodejs.ts
@@ -0,0 +1,58 @@
+import {
+  NodejsFunction as LambdaNodejsFunction,
+  NodejsFunctionProps
+} from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Construct } from 'constructs'
+
+const getNameFromRuntime = (runtime: Runtime): string => runtime.name
+
+/**
+ * Valid runtimes, does not contain custom runtimes
+ */
+export const validRuntimes = [
+  Runtime.NODEJS_14_X
+]
+
+const blackList: string[] = [
+  Runtime.NODEJS,
+  Runtime.NODEJS_4_3,
+  Runtime.NODEJS_6_10,
+  Runtime.NODEJS_8_10,
+  Runtime.NODEJS_10_X,
+  Runtime.NODEJS_12_X
+].map(getNameFromRuntime)
+
+const latestVersions: Record<string, string> = {
+  [getNameFromRuntime(Runtime.NODEJS)]: 'NODEJS_14_X',
+  [getNameFromRuntime(Runtime.NODEJS_4_3)]: 'NODEJS_14_X',
+  [getNameFromRuntime(Runtime.NODEJS_6_10)]: 'NODEJS_14_X',
+  [getNameFromRuntime(Runtime.NODEJS_8_10)]: 'NODEJS_14_X',
+  [getNameFromRuntime(Runtime.NODEJS_10_X)]: 'NODEJS_14_X',
+  [getNameFromRuntime(Runtime.NODEJS_12_X)]: 'NODEJS_14_X'
+}
+
+/**
+ * Compliant NodeJS Lambda Function.
+ *
+ * See README for usage examples
+ */
+export class NodejsFunction extends LambdaNodejsFunction {
+  constructor (scope: Construct, id: string, props: NodejsFunctionProps) {
+    super(scope, id, props)
+    this.node.addValidation({
+      validate: () => {
+        return [
+          ...this.checkRuntime()
+        ]
+      }
+    })
+  }
+
+  protected checkRuntime () {
+    const runtime = getNameFromRuntime(this.runtime)
+    return (blackList.includes(runtime))
+      ? [`Lambda runtime must be latest runtime available for language. Found ${runtime}, please use ${latestVersions[runtime]} instead`]
+      : []
+  }
+}
---
+++ test/lambda-nodejs.test.ts
@@ -0,0 +1,39 @@
+// to be tested
+import { NodejsFunction } from '../lib/lambda-nodejs'
+
+// tools
+import '@aws-cdk/assert/jest'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Stack } from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import * as path from 'path'
+
+describe('Lambda NodeJS', () => {
+  describe('NodejsFunction', () => {
+    it('should create a lambda', () => {
+      const stack = new Stack()
+
+      new NodejsFunction(stack, 'Function', {
+        handler: 'handler',
+        entry: path.join(__dirname, 'hello-world.ts')
+      })
+
+      expect(stack).toHaveResource('AWS::Lambda::Function', {
+        Handler: 'index.handler',
+        Runtime: 'nodejs14.x'
+      })
+    })
+
+    it('should throw a relevant error message for bad runtime - NodeJS', () => {
+      const stack = new Stack()
+
+      new NodejsFunction(stack, 'Function', {
+        runtime: Runtime.NODEJS_12_X,
+        handler: 'handler',
+        entry: path.join(__dirname, 'hello-world.ts')
+      })
+
+      expect(() => { Template.fromStack(stack) }).toThrow('Lambda runtime must be latest runtime available for language. Found nodejs12.x, please use NODEJS_14_X instead')
+    })
+  })
+})
