4dd00d7521d5ad13283a437c93a17bebee8bddf7
Urban Pettersson <urbanpe88@gmail.com>
2022-03-21 13:10:43 +0100

    feat: add tagging functions for S3 bucket exemptions

--- lib/tags.ts
+++ lib/tags.ts
@@ -1,4 +1,5 @@
 import { Tags } from 'aws-cdk-lib'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs'
 
 /**
@@ -47,4 +48,27 @@ export enum BackupPlan {
  */
 export const enableBackups = (construct: Construct, backupPlan: BackupPlan = BackupPlan.STANDARD): void => {
   Tags.of(construct).add('BackupPlan', backupPlan)
+}
+
+/**
+ * Static value used for exemption tags
+ */
+export const ExemptionValue = 'Exempt'
+
+/**
+ * Tags a Bucket as exempt from the auto fixing of https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#fsbp-s3-5 which is handled within Enfo managed accounts
+ * @param bucket
+ * CDK Bucket Construct
+ */
+export const exemptBucketFromSslAutoFix = (bucket: Bucket): void => {
+  Tags.of(bucket).add('SecureTransportAutomation', ExemptionValue)
+}
+
+/**
+ * Tags a Bucket as exempt from the auto fixing of https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#fsbp-s3-2 which is handled within Enfo managed accounts
+ * @param bucket
+ * CDK Bucket Construct
+ */
+export const exemptBucketFromBlockPublicAutoFix = (bucket: Bucket): void => {
+  Tags.of(bucket).add('BlockPublicAccessAutomation', ExemptionValue)
 }
